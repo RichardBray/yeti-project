f7645b54102355690ef32d973eba4a5f5178e703
Richard Bray <richardbray89@gmail.com>
2021-03-02 21:39:40 +0000

    chore: remove version getting version from package

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,11 +5,10 @@ import flixel.text.FlxText;
 import utils.LoadFile;
 
 class PlayState extends FlxState {
-	final pjson = LoadFile.json("/Users/richardoliverbray/yeti-project/package.json");
-
+	// final pjson = LoadFile.json("/Users/richardoliverbray/yeti-project/package.json");
 	override public function create() {
 		super.create();
-		add(new FlxText('Hello World ${pjson.version}', 32).screenCenter());
+		add(new FlxText('Hello World', 32).screenCenter());
 	}
 
 	override public function update(elapsed:Float) {
