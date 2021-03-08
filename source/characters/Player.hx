aa73e88b310892f86026cb6e2b1b87183e6907cd
Richard Bray <richardbray89@gmail.com>
2021-03-08 21:16:39 +0000

    feat: add basic player movement

--- source/characters/Player.hx
+++ source/characters/Player.hx
@@ -1,11 +1,77 @@
 package characters;
 
+import flixel.FlxObject;
 import flixel.FlxSprite;
 import utils.Colors;
+import utils.Controls;
+
+enum States {
+	Sneaking;
+	Running;
+	Idle;
+}
 
 final class Player extends FlxSprite {
+	final SNEAK_SPEED = 125;
+	final RUN_SPEED = 250;
+	final controls:Controls = Controls.instance;
+
+	var playerState:States = States.Idle;
+
+	// - controls
+	var left = false;
+	var right = false;
+	var circle = false;
+	// - control mods
+	var bothDirectionsPressed = false;
+	var singleDirectionPressed = false;
+	var noDirectionPressed = false;
+
 	public function new(x:Float = 0, y:Float = 0) {
 		super(x, y);
-		makeGraphic(100, 100, Colors.white);
+		makeGraphic(100, 200, Colors.white);
+		drag.x = RUN_SPEED * 4;
+
+		setFacingFlip(FlxObject.LEFT, true, false);
+		setFacingFlip(FlxObject.RIGHT, false, false);
+	}
+
+	function movement(speed:Int) {
+		if (bothDirectionsPressed || noDirectionPressed) {
+			playerState = States.Idle;
+		} else {
+			velocity.x = right ? speed : -speed;
+			facing = right ? FlxObject.RIGHT : FlxObject.LEFT;
+		}
+	}
+
+	function stateMachine() {
+		switch (playerState) {
+			case States.Sneaking:
+				movement(SNEAK_SPEED);
+				if (circle) playerState = States.Running;
+			case States.Running:
+				movement(RUN_SPEED);
+				if (!circle) playerState = States.Sneaking;
+			case States.Idle:
+				velocity.x = 0;
+				if (singleDirectionPressed) playerState = States.Sneaking;
+		}
+	}
+
+	function updateControls() {
+		left = controls.left.check();
+		right = controls.right.check();
+		circle = controls.circle.check();
+
+		bothDirectionsPressed = left && right;
+		singleDirectionPressed = left || right;
+		noDirectionPressed = !singleDirectionPressed;
+	}
+
+	override function update(elapsed:Float) {
+		super.update(elapsed);
+		updateControls();
+		stateMachine();
 	}
 }
--- source/utils/Controls.hx
+++ source/utils/Controls.hx
@@ -4,4 +4,30 @@ import flixel.input.actions.FlxAction.FlxActionDigital;
 
 final class Controls {
 	public var left:FlxActionDigital;
+	public var right:FlxActionDigital;
+	public var circle:FlxActionDigital;
+
+	public static final instance:Controls = new Controls();
+
+	private function new() {
+		initInputs();
+		keyboardInputs();
+	}
+
+	function initInputs() {
+		left = new FlxActionDigital();
+		right = new FlxActionDigital();
+		circle = new FlxActionDigital();
+	}
+
+	function keyboardInputs() {
+		// - left
+		left.addKey(LEFT, PRESSED);
+		left.addKey(A, PRESSED);
+		// - right
+		right.addKey(RIGHT, PRESSED);
+		right.addKey(D, PRESSED);
+		// - circle
+		circle.addKey(SHIFT, PRESSED);
+	}
 }
