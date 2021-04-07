d8814612f8bb0274a9d440f162cf50b0d65b3e15
Richard Bray <richardbray89@gmail.com>
2021-04-07 09:59:24 +0100

    chore: update html5 target resolution

--- src/Main.hx
+++ src/Main.hx
@@ -7,6 +7,8 @@ import openfl.display.Sprite;
 class Main extends Sprite {
 	public function new() {
 		super();
-		addChild(new FlxGame(levels.SkiLevel, true));
+		// Resolution explicitly added here to enable game to scale properly
+		// in different resolutions.
+		addChild(new FlxGame(1920, 1080, levels.SkiLevel, true));
 	}
 }
